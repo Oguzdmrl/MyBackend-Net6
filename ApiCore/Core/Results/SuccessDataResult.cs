b8c07523300be16366b37d101c5f8ce3a77fec11
Oğuz Demirel <110036091+Oguzdmrl@users.noreply.github.com>
2023-03-06 13:58:48 +0300

    ResponseResult yapısı

---
+++ ApiCore/Core/Results/SuccessDataResult.cs
@@ -0,0 +1,32 @@
+﻿namespace Core.Results
+{
+    public sealed partial class ResponseDataResult<T>
+    {
+        public IEnumerable<T> ListResponseModel { get; set; } = null;
+        public T ResponseModel { get; set; }
+        public bool Status { get; set; } = false;
+        public string Message { get; set; }
+        public long ModelCount { get; set; } = 0;
+        public ResponseDataResult()
+        {
+        }
+        public ResponseDataResult(IEnumerable<T> ListResponseModel, bool Status, string Message, long ModelCount)
+        {
+            this.ListResponseModel = ListResponseModel;
+            this.Status = Status;
+            this.Message = Message;
+            this.ModelCount = ModelCount;
+        }
+        public ResponseDataResult(T ResponseModel, bool Status, string Message)
+        {
+            this.ResponseModel = ResponseModel;
+            this.Status = Status;
+            this.Message = Message;
+        }
+        public ResponseDataResult( bool Status, string Message)
+        {
+            this.Status = Status;
+            this.Message = Message;
+        }
+    }
+}
