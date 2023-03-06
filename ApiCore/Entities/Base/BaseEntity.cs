5811183d5d9d06bdd1df32f89cf6503562cd2259
Oğuz Demirel <110036091+Oguzdmrl@users.noreply.github.com>
2023-03-06 12:53:14 +0300

    Klasörleme yapıldı

---
+++ ApiCore/Entities/Base/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Base
+{
+    public class BaseEntity<T>
+    {
+        public T ID { get; set; }
+        public DateTime Created_Date { get; set; } = DateTime.Now;
+        public DateTime Updated_Date { get; set; }
+    }
+}
---
+++ ApiCore/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿using Entities.Base;
+
+namespace Entities
+{
+    public class Category : BaseEntity<Guid>
+    {
+        public string Name { get; set; }
+    }
+}
