25eec9a28113bd0be1967f159256b8fa0173246d
Oğuz Demirel <110036091+Oguzdmrl@users.noreply.github.com>
2023-03-09 09:54:44 +0300

    BulkInsert-AddrangeDelete-Update eklendi

--- ApiCore/DataAccess/Repo/BaseRepository/CommandRepository/CommandRepository.cs
+++ ApiCore/DataAccess/Repo/BaseRepository/CommandRepository/CommandRepository.cs
@@ -17,22 +17,86 @@ namespace DataAccess.Repo.BaseRepository.CommandRepository
         }
         public virtual async Task<ResponseDataResult<T>> InsertAsync(T entity)
         {
-            EntityEntry<T> result = await _context.Set<T>().AddAsync(entity);
-            await _uow.SaveChangesAsync();
-            return await Task.FromResult(new ResponseDataResult<T>() { ResponseModel = result.Entity, Status = true, Message = "Kayıt İşlemi Başarılı." });
+            try
+            {
+                EntityEntry<T> result = await _context.Set<T>().AddAsync(entity);
+                await _uow.SaveChangesAsync();
+                return await Task.FromResult(new ResponseDataResult<T>() { ResponseModel = result.Entity, Status = true, Message = "Kayıt İşlemi Başarılı." });
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseDataResult<T>() { Status = false, Message = $"Hata. {ex}" });
+            }
         }
         public virtual async Task<ResponseDataResult<T>> UpdateAsync(T entity)
         {
-            EntityEntry<T> result = _context.Entry(entity);
-            result.State = EntityState.Modified;
-            await _uow.SaveChangesAsync();
-            return await Task.FromResult(new ResponseDataResult<T>() { ResponseModel = result.Entity, Status = true, Message = "Güncelleme İşlemi Başarılı." });
+            try
+            {
+                EntityEntry<T> result = _context.Entry(entity);
+                result.State = EntityState.Modified;
+                await _uow.SaveChangesAsync();
+                return await Task.FromResult(new ResponseDataResult<T>() { ResponseModel = result.Entity, Status = true, Message = "Güncelleme İşlemi Başarılı." });
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseDataResult<T>() { Status = false, Message = $"Hata. {ex}" });
+            }
         }
         public virtual async Task<ResponseDataResult<T>> DeleteAsync(T entity)
         {
-            EntityEntry<T> result = _context.Set<T>().Remove(entity);
-            await _uow.SaveChangesAsync();
-            return await Task.FromResult(new ResponseDataResult<T>() { ResponseModel = result.Entity, Status = true, Message = "Silme İşlemi Başarılı." });
+            try
+            {
+                EntityEntry<T> result = _context.Set<T>().Remove(entity);
+                await _uow.SaveChangesAsync();
+                return await Task.FromResult(new ResponseDataResult<T>() { ResponseModel = result.Entity, Status = true, Message = "Silme İşlemi Başarılı." });
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseDataResult<T>() { Status = false, Message = $"Hata. {ex}" });
+            }
         }
+      
+        #region RangeAsync
+        public virtual async Task<ResponseDataResult<T>> BulkInsertAsync(IEnumerable<T> entity)
+        {
+            try
+            {
+                await _context.Set<T>().AddRangeAsync(entity);
+                await _uow.SaveChangesAsync();
+                return await Task.FromResult(new ResponseDataResult<T>() { Status = true, Message = "Toplu Kayıt İşlemi Başarılı." });
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseDataResult<T>() { Status = false, Message = $"Hata. {ex}" });
+            }
+        }
+        public virtual async Task<ResponseDataResult<T>> RemoveRangeAsync(IEnumerable<T> entity)
+        {
+            try
+            {
+                _context.Set<T>().RemoveRange(entity);
+                await _uow.SaveChangesAsync();
+                return await Task.FromResult(new ResponseDataResult<T>() { Status = true, Message = "Toplu Silme İşlemi Başarılı." });
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseDataResult<T>() { Status = false, Message = $"Hata. {ex}" });
+            }
+        }
+        public virtual async Task<ResponseDataResult<T>> UpdateRangeAsync(IEnumerable<T> entity)
+        {
+            try
+            {
+                _context.Set<T>().UpdateRange(entity);
+                await _uow.SaveChangesAsync();
+                return await Task.FromResult(new ResponseDataResult<T>() { Status = true, Message = "Toplu Güncelleme İşlemi Başarılı." });
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new ResponseDataResult<T>() { Status = false, Message = $"Hata. {ex}" });
+            }
+        }
+        #endregion
+
     }
 }
--- ApiCore/DataAccess/Repo/BaseRepository/CommandRepository/ICommandRepository.cs
+++ ApiCore/DataAccess/Repo/BaseRepository/CommandRepository/ICommandRepository.cs
@@ -6,7 +6,10 @@ namespace DataAccess.Repo.BaseRepository.CommandRepository
     public interface ICommandRepository<T> where T : BaseEntity<Guid>
     {
         Task<ResponseDataResult<T>> InsertAsync(T entity);
+        Task<ResponseDataResult<T>> BulkInsertAsync(IEnumerable<T> entity);
         Task<ResponseDataResult<T>> UpdateAsync(T entity);
         Task<ResponseDataResult<T>> DeleteAsync(T entity);
+        Task<ResponseDataResult<T>> RemoveRangeAsync(IEnumerable<T> entity);
+        Task<ResponseDataResult<T>> UpdateRangeAsync(IEnumerable<T> entity);
     }
 }
